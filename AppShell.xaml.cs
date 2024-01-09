1b3fb47e7762c24e9450c4a180f30a3e00ae5327
master <master@SMLAB306PC16_23>
2024-01-08 19:36:40 -0500

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MoisesSanchezExamen
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Vistas/Login.xaml.cs
@@ -0,0 +1,19 @@
+namespace MoisesSanchezExamen.Vistas;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+
+    private void btnIniciar_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnAcerca_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
